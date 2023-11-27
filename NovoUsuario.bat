35c61621e9ff6f70c169b592f83e0423c983aa2b
luccabugatti <lbugattilive@gmail.com>
2023-11-26 23:17:36 -0300

    Exclusão de arquivos desnecessarios

--- NovoUsuario.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd C:\Users\lucca.vasconcelos\Desktop\A3 Sistemas Distribuidos\src\bin
-java Usuario
--- src/compilar.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-javac -d bin Usuario.java
-javac -d bin Servidor.java
-javac -d bin Conexao.java
